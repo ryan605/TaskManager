8ca01facdd5a6f6bd0c89eab9d5b04483077b447
ryan605 <langatryan26@gmail.com>
2023-03-22 10:54:40 +0300

    validations added

--- db/migrate/20230322074112_create_users.rb
+++ db/migrate/20230322074112_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :username, null: false, unique: true
       t.string :email, null: false, unique: true
-      t.string :password_digest
+      t.string :password_digest, null: false
 
       t.timestamps
     end
